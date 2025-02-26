1ff17544e3b74d9f276abadcaecf07fe3bb2db22
brithvik0305 <122633165+brithvik0305@users.noreply.github.com>
2025-02-26 21:39:52 +0530

    Delete prisma/migrations directory

--- prisma/migrations/20250226070757_1/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `FriendEmail` on the `referral` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `referral` DROP COLUMN `FriendEmail`,
-    ADD COLUMN `FriendMail` VARCHAR(191) NOT NULL DEFAULT 'Unknown';
